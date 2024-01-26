44e0f2bf9e066bdfd028181ac31b2ac0c1a4874b
developer.mezbah <developer.mezbah@gmail.com>
2024-01-26 19:11:56 +0600

    update readme

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,245 +1,285 @@
-
-
 const loading = () => {
   return (
-    <div className="loading-body">
-      <div class="loading-container">
-        <div class="row">
-          <div class="loader">
-            <div class="lines">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-          <div class="loader">
-            <div class="lines">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-          <div class="loader">
-            <div class="lines">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-          <div class="loader">
-            <div class="lines">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-          <div class="loader">
-            <div class="lines">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
+    // <div className="loading-body">
+    //   <div class="loading-container">
+    //     <div class="row">
+    //       <div class="loader">
+    //         <div class="lines">
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //         </div>
+    //       </div>
+    //       <div class="loader">
+    //         <div class="lines">
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //         </div>
+    //       </div>
+    //       <div class="loader">
+    //         <div class="lines">
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //         </div>
+    //       </div>
+    //       <div class="loader">
+    //         <div class="lines">
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //         </div>
+    //       </div>
+    //       <div class="loader">
+    //         <div class="lines">
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //         </div>
+    //       </div>
+    //     </div>
+    //     <div class="row">
+    //       <div class="loader">
+    //         <div class="lines">
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //         </div>
+    //       </div>
+    //       <div class="loader">
+    //         <div class="lines">
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //         </div>
+    //       </div>
+    //       <div class="loader">
+    //         <div class="lines">
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //         </div>
+    //       </div>
+    //       <div class="loader">
+    //         <div class="lines">
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //         </div>
+    //       </div>
+    //       <div class="loader">
+    //         <div class="lines">
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //         </div>
+    //       </div>
+    //     </div>
+    //     <div class="row">
+    //       <div class="loader">
+    //         <div class="lines">
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //         </div>
+    //       </div>
+    //       <div class="loader">
+    //         <div class="lines">
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //         </div>
+    //       </div>
+    //       <div class="loader">
+    //         <div class="lines">
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //         </div>
+    //       </div>
+    //       <div class="loader">
+    //         <div class="lines">
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //         </div>
+    //       </div>
+    //       <div class="loader">
+    //         <div class="lines">
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //         </div>
+    //       </div>
+    //     </div>
+    //     <div class="row">
+    //       <div class="loader">
+    //         <div class="lines">
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //         </div>
+    //       </div>
+    //       <div class="loader">
+    //         <div class="lines">
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //         </div>
+    //       </div>
+    //       <div class="loader">
+    //         <div class="lines">
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //         </div>
+    //       </div>
+    //       <div class="loader">
+    //         <div class="lines">
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //         </div>
+    //       </div>
+    //       <div class="loader">
+    //         <div class="lines">
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //         </div>
+    //       </div>
+    //     </div>
+    //     <div class="row">
+    //       <div class="loader">
+    //         <div class="lines">
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //         </div>
+    //       </div>
+    //       <div class="loader">
+    //         <div class="lines">
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //         </div>
+    //       </div>
+    //       <div class="loader">
+    //         <div class="lines">
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //         </div>
+    //       </div>
+    //       <div class="loader">
+    //         <div class="lines">
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //         </div>
+    //       </div>
+    //       <div class="loader">
+    //         <div class="lines">
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //           <span></span>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+
+    <div
+      role="status"
+      class="w-screen h-screen container mx-auto p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700"
+    >
+      <div class="flex items-center justify-between">
+        <div>
+          <div class="h-[10vh] bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+          <div class="w-32 h-[5vh] bg-gray-200 rounded-full dark:bg-gray-700"></div>
         </div>
-        <div class="row">
-          <div class="loader">
-            <div class="lines">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-          <div class="loader">
-            <div class="lines">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-          <div class="loader">
-            <div class="lines">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-          <div class="loader">
-            <div class="lines">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-          <div class="loader">
-            <div class="lines">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="loader">
-            <div class="lines">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-          <div class="loader">
-            <div class="lines">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-          <div class="loader">
-            <div class="lines">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-          <div class="loader">
-            <div class="lines">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-          <div class="loader">
-            <div class="lines">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="loader">
-            <div class="lines">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-          <div class="loader">
-            <div class="lines">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-          <div class="loader">
-            <div class="lines">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-          <div class="loader">
-            <div class="lines">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-          <div class="loader">
-            <div class="lines">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="loader">
-            <div class="lines">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-          <div class="loader">
-            <div class="lines">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-          <div class="loader">
-            <div class="lines">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-          <div class="loader">
-            <div class="lines">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-          <div class="loader">
-            <div class="lines">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-        </div>
+        <div class="h-[5vh] bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
       </div>
+      <div class="flex items-center justify-between pt-4">
+        <div>
+          <div class="h-[5vh] bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+          <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+        </div>
+        <div class="h-[5vh] bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+      </div>
+      <div class="flex items-center justify-between pt-4">
+        <div>
+          <div class="h-[5vh] bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+          <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+        </div>
+        <div class="h-[5vh] bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+      </div>
+      <div class="flex items-center justify-between pt-4">
+        <div>
+          <div class="h-[5vh] bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+          <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+        </div>
+        <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+      </div>
+      <div class="flex items-center justify-between pt-4">
+        <div>
+          <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+          <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+        </div>
+        <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+      </div>
+      <span class="sr-only">Loading...</span>
     </div>
   );
 };
